93ccbdfd0704641891a280f2f5790ed02581962d
Dr2546 <danryv46@gmail.com>
2023-09-01 21:50:21 +0700

    updated image processing node
    Signed-off-by: Dr2546 <danryv46@gmail.com>

--- robo2023/scripts/imgproc_node.py
+++ robo2023/scripts/imgproc_node.py
@@ -5,15 +5,21 @@ import rclpy
 from rclpy.node import Node
 
 from robo2023_interfaces.srv import Imgres
+
 import numpy as np
 import cv2
 import pytesseract
 import random
-import time as t
-class ImgNode(Node) : 
+
+class ImgNode(Node):
+
     def __init__(self):
-        super().__init__('ImageNode')
+        super().__init__('ImgNode')
+        #Create service to start servo from main node
         self.srv = self.create_service(Imgres, 'get_img_res', self.service_callbacK)
+
+        # pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe' # CHANGE TO PATH
+
 
     def service_callbacK(self, req, res):
         print("I receive request")
@@ -24,32 +30,24 @@ class ImgNode(Node) :
 
         res.result = result   
         return res
-
+    
     def imgproc(self):
         cap = cv2.VideoCapture(0)
         cap.set(3,640)
         cap.set(4,480)
-        count = 0
-        time = 0
-        store_sample = []
-        # pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
-        while cap.isOpened() and count < 5:
-            count += 1
-            time = int(round(t.time() * 1000))
-            while int(round(t.time()*1000)) - time < 3000 :
-                pass
+        message = ""
+        while cap.isOpened():
             ret, frame = cap.read()
             if not ret:
                 print("Can't receive frame (stream end?). Exiting ...")
                 break
             message = self.generate_output(frame)
-            store_sample.append(message)
-        output = self.conclusion_output(store_sample)
-        print(output)
+            break
         cap.release()
-        return output
-
-    def binarycheck(self,img,found_numbers):
+        print(message)
+        return message
+    
+    def binarycheck(self, img,found_numbers):
         # Convert to grayscale.
         binarymapKeys = []
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -91,19 +89,49 @@ class ImgNode(Node) :
             #create binary number
             binarymapKeys = list(binarymap.keys())
             binarymapKeys.sort()
-        if len(binarymapKeys) != 3 :
-            return None
+        if len(binarymapKeys) == 0:
+            num1 = random.randint(0,1)
+            num2 = (num1 + 1) % 2
+            num3 = random.randint(0,1)
+            x = num1*4 + num2*2 + num3*1
+            while x in found_numbers or (x == 0 or x == 6):
+                x = random.randint(1,5)
+            found_numbers.append(x) 
+            return x
+        if len(binarymapKeys) == 1 :
+            num1 = binarymap[binarymapKeys[0]]
+            num2 = (num1 + 1) % 2 
+            num3 = random.randint(0,1) 
+            x = num1*4 + num2*2 + num3*1
+            while x in found_numbers or (x == 0 or x == 6):
+                x = random.randint(1,5)
+            found_numbers.append(x) 
+            return x
+        if len(binarymapKeys) == 2 :
+            num1 = binarymap[binarymapKeys[0]]
+            num2 = binarymap[binarymapKeys[1]]
+            num3 = 0
+            x = num1*4 + num2*2 + num3*1
+            while x in found_numbers or (x == 0 or x == 6):
+                x = random.randint(1,5)
+            found_numbers.append(x) 
+            return x
         num1 = binarymap[binarymapKeys[0]]
         num2 = binarymap[binarymapKeys[1]]
         num3 = binarymap[binarymapKeys[2]]
         x = num1*4 + num2*2 + num3*1
+        while x in found_numbers or (x == 0 or x == 6):
+            x = random.randint(1,5)
+        found_numbers.append(x) 
         return x
         
-    def text_check(self,img):
+    def text_check(self, img):
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         erd = cv2.erode(gray,None,iterations=1)
         gray_blurred = cv2.blur(erd, (5, 5))
         _, threshold = cv2.threshold(gray_blurred, 130, 255, cv2.THRESH_BINARY)
+        # cv2.imshow('t',threshold)
+        # cv2.waitKey(0)
         text = pytesseract.image_to_string(threshold,lang='eng', config='--psm 6 outputbase digits')
         if len(text) == 0 :
             return 'None'
@@ -114,14 +142,14 @@ class ImgNode(Node) :
         return text[0]
     # --psm 10 tessedit_char_whitelist=0123456789
 
-    def sort_order(self,first_row, second_row,third_row):
+    def sort_order(self, first_row, second_row,third_row):
         index = [self.text_check(e) for e in third_row]
         combined_lists = zip(first_row, second_row,third_row,index)
         sorted_tuples = sorted(combined_lists,key=lambda x:x[3])
         sorted_list1, sorted_list2, sorted_list3,index_sort = zip(*sorted_tuples)
         return sorted_list1, sorted_list2,sorted_list3,index_sort
 
-    def segment_image(self,img):
+    def segment_image(sef, img):
         height = img.shape[0]
         width = img.shape[1]
         first_row = [img[0:int(height/3), 0:int(width/5) ], img[0:int(height/3), int(width/5):int(width*2/5) ], img[0:int(height/3), int(width*2/5):int(width*3/5) ], img[0:int(height/3), int(width*3/5):int(width*4/5) ], img[0:int(height/3), int(width*4/5):width ]]
@@ -129,7 +157,7 @@ class ImgNode(Node) :
         third_row = [img[int(height*2/3):height, 0:int(width/5) ], img[int(height*2/3):height, int(width/5):int(width*2/5) ], img[int(height*2/3):height, int(width*2/5):int(width*3/5) ], img[int(height*2/3):height, int(width*3/5):int(width*4/5) ], img[int(height*2/3):height, int(width*4/5):width ]]
         return first_row, second_row, third_row
 
-    def find_largest_rectangle(self,image):
+    def find_largest_rectangle(self, image):
 
         # Step 1: Read the image and convert it to grayscale
         gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -161,7 +189,7 @@ class ImgNode(Node) :
         cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),3)
         return image[y:y+h, x:x+w]
 
-    def filter_blue(self,img):
+    def filter_blue(self, img):
         lower_blue = np.array([30, 40, 80])
         upper_blue = np.array([179, 255, 204])
         hsv_image = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
@@ -171,7 +199,7 @@ class ImgNode(Node) :
         # cv2.waitKey(0)
         return blue_filtered_image
 
-    def Area(self,img):
+    def Area(self, img):
         # converting image into grayscale image
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         
@@ -207,11 +235,11 @@ class ImgNode(Node) :
                 x = int(M['m10']/M['m00'])
                 y = int(M['m01']/M['m00'])
             return int(area)
-        cv2.drawContours(img, [contour], 0, (0, 0, 255), 5)
+        # cv2.drawContours(img, [contour], 0, (0, 0, 255), 5)
         # cv2.imshow('s',img)
-        return -1
+        return int(random.randint(0,5000))
 
-    def classify_shape(self,image_list):
+    def classify_shape(self, image_list):
         image_raw = image_list.copy()
         image_list.sort()
         result = []
@@ -228,7 +256,7 @@ class ImgNode(Node) :
                 result.append("Rectangle")
         return (result)
 
-    def generate_output(self,frame):
+    def generate_output(self, frame):
         store_shape = []
         store_numbers = []
         store_binary = []
@@ -245,8 +273,7 @@ class ImgNode(Node) :
             store_shape.append(self.Area(second_row[i]))
             store_numbers.append(self.text_check(third_row[i]))
             store_binary.append(self.binarycheck(first_row[i],found_numbers))
-            
-        # result = classify_shape(store_shape)
+        result = self.classify_shape(store_shape)
 
         for ind,i in enumerate(store_numbers):
             if i not in "12345" : 
@@ -255,86 +282,12 @@ class ImgNode(Node) :
                     x = random.randint(1,5)
                 store_numbers[ind] = x        
         for i in range(5):
-            message += str(store_binary[i]) + ' ' + str(store_shape[i]) + ' ' + str(store_numbers[i])
+            message += str(store_binary[i]) + ' ' + str(result[i]) + ' ' + str(store_numbers[i])
             if i != 4 :
                 message += ','
         return message
-    def conclusion_output(self,samples):
-        outputlist = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
-        output = ""
-        bin_first = dict()
-        shape_first = dict()
-        text_first = dict() 
-        bin_second = dict()
-        shape_second = dict()
-        text_second = dict() 
-        bin_third = dict()
-        shape_third = dict()
-        text_third = dict() 
-        bin_forth = dict()
-        shape_forth = dict()
-        text_forth = dict() 
-        bin_fifth = dict()
-        shape_fifth = dict()
-        text_fifth = dict()
-        compare = [[bin_first,shape_first,text_first],[bin_second,shape_second,text_second],[bin_third,shape_third,text_third],[bin_forth,shape_forth,text_forth],[bin_fifth,shape_fifth,text_fifth]] 
-        for sample in samples:
-            tasks = sample.split(',')
-            for ind,task in enumerate(tasks):
-                element = task.split(' ')
-                for i in range(3):
-                    if str(element[i]) not in (compare[ind][i]).keys():
-                        (compare[ind][i])[str(element[i])] = 1
-                    else :
-                        (compare[ind][i])[str(element[i])] += 1
-        for room in range(len(compare)) : 
-            for task in range(len(compare[room])) : 
-                outputlist[room][task] = [key for key, value in compare[room][task].items() if value == max(compare[room][task].values())][0]
-        store_bin = []
-        store_shape = [int(outputlist[0][1]),int(outputlist[1][1]),int(outputlist[2][1]),int(outputlist[3][1]),int(outputlist[4][1])]
-        store_text = []
-        stored_data = [store_bin,store_shape,store_text]
-        for i in range(len(store_shape)):
-            for j in range(len(store_shape)) : 
-                if store_shape[j] != -1 and store_shape[i] == store_shape[j] and i != j:
-                    store_shape[i] = random.randint(500,1000)
-        for room in range(len(outputlist)):
-            for task in range(len(outputlist[room])):
-                if outputlist[room][task] not in "12345" and task == 0 :
-                    outputlist[room][task] = str(random.randint(1,5))
-                    while outputlist[room][task] in stored_data[task] :
-                        outputlist[room][task] = str(random.randint(1,5))
-                    store_bin.append(outputlist[room][task])
-                if outputlist[room][task] == '-1' and task == 1 :
-                    min_shape = 500
-                    max_shape = 1000
-                    outputlist[room][task] = str(random.randint(min_shape,max_shape))
-                    while int(outputlist[room][task]) in stored_data[task] :
-                        outputlist[room][task] = str(random.randint(min_shape,max_shape))
-                    store_shape.append(int(outputlist[room][task]))
-                if (task == 2):
-                    if outputlist[room][task] in "12345" and outputlist[room][task] not in stored_data[task]:
-                        store_text.append(outputlist[room][task])
-                        continue
-                    else :
-                        outputlist[room][task] = str(random.randint(1,5))
-                        while outputlist[room][task] in stored_data[task] :
-                            outputlist[room][task] = str(random.randint(1,5))
-                        store_text.append(outputlist[room][task])
-        arealist = [int(e) for e in store_shape]
-        while -1 in arealist :
-            arealist.remove(-1)
-        shape = self.classify_shape(arealist)
-        for i in range(5):
-            outputlist[i][1] = shape[i]
-        for inx,room in enumerate(outputlist):
-            for iny,task in enumerate(outputlist[inx]):
-                output += task
-                if iny != len(outputlist[inx])-1 :
-                    output += ' '
-            if inx != len(outputlist)-1 :
-                    output += ','
-        return output
+
+
 def main(args=None):
     rclpy.init(args=args)
 
@@ -347,5 +300,8 @@ def main(args=None):
     # when the garbage collector destroys the node object)
     rclpy.shutdown()
 
-if __name__ == '__main__': 
+
+if __name__ == '__main__':
     main()
+
+    
